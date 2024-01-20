c5dc37f11e55aa92cfa5fd56a97d8d4461a3c5ee
gritNgo <dustspeck00@gmail.com>
2024-01-20 16:00:02 +0100

    Single post page styling complete

--- src/app/blog/[slug]/page.jsx
+++ src/app/blog/[slug]/page.jsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import styles from "./singlePost.module.css";
 
 const SinglePostPage = () => {
@@ -5,6 +6,7 @@ const SinglePostPage = () => {
     <div className={styles.container}>
       <div className={styles.imgContainer}>
         <Image
+          className={styles.img}
           src="https://images.pexels.com/photos/41953/earth-blue-planet-globe-planet-41953.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
           alt=""
           fill
@@ -12,6 +14,29 @@ const SinglePostPage = () => {
       </div>
       <div className={styles.textContainer}>
         <h1 className={styles.title}>Title</h1>
+        <div className={styles.detail}>
+          <Image
+            className={styles.avatar}
+            src="https://images.pexels.com/photos/47367/full-moon-moon-bright-sky-47367.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            alt=""
+            width={50}
+            height={50}
+          />
+          <div className={styles.detailText}>
+            <span className={styles.detailTitle}>Author</span>
+            <span className={styles.detailValue}>John Smith</span>
+          </div>
+          <div className={styles.detailText}>
+            <span className={styles.detailTitle}>Published</span>
+            <span className={styles.detailValue}>25/01/2024</span>
+          </div>
+        </div>
+        <div className={styles.content}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur
+          dolorem ducimus culpa quam eligendi architecto laborum quis aliquid
+          harum, magnam sequi iste ipsum alias sapiente reiciendis voluptas
+          illum aliquam! Labore?
+        </div>
       </div>
     </div>
   );
