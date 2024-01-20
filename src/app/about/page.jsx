ba47243f3c60a2889c6925f50a1a95b29f754287
gritNgo <dustspeck00@gmail.com>
2024-01-20 11:09:05 +0100

    Configured to take external images

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,9 +1,18 @@
+import styles from "./about.module.css";
+import Image from "next/image";
+
 const AboutPage = () => {
   return (
     <div>
-      AboutPage
+      <div className={styles.imgContainer}>
+        <Image
+          src="https://images.pexels.com/photos/1252890/pexels-photo-1252890.jpeg?auto=compress&cs=tinysrgb&w=400"
+          alt="About page image"
+          fill
+        />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default AboutPage
+export default AboutPage;
