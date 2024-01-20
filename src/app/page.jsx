295699663e43457383abfe34bbe17f0d83e7eaab
gritNgo <dustspeck00@gmail.com>
2024-01-20 11:56:29 +0100

    Home page styling complete

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,7 +1,32 @@
+import Image from "next/image";
+import styles from "./home.module.css";
+
 const Home = () => {
   return (
-    <div>Homepage</div>
-  )
-}
+    <div className={styles.container}>
+      <div className={styles.textContainer}>
+        <h1 className={styles.title}>Title of homepage</h1>
+        <p className={styles.desc}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente
+          alias quasi facilis, tenetur hic ut nulla doloremque distinctio quo
+          id! Quidem provident iusto pariatur quas aliquid cumque quam nostrum
+          nemo!
+        </p>
+        <div className={styles.buttons}>
+          <button className={styles.button}>Learn More</button>
+          <button className={styles.button}>Contact</button>
+        </div>
+        <div className={styles.brands}>
+          <Image src="/brands.png" alt="" fill className={styles.brandImg} />
+        </div>
+      </div>
+      <div className={styles.imgContainer}>
+        <Image src="/hero.gif" alt="" fill className={styles.heroImg} />
+      </div>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
+
+// fill attribute used to fill image container
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,8 +1,12 @@
+import styles from "./footer.module.css";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <div className={styles.container}>
+      <div className={styles.logo}>My NEXT app</div>
+      <div className={styles.text}>My Next App © All rights reserved.</div>
+    </div>
+  );
+};
 
-export default Footer
+export default Footer;
