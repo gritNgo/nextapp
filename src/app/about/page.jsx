f8b99743afcbc8d724daa49c6477545eb8c7e4d0
gritNgo <dustspeck00@gmail.com>
2024-01-20 14:22:13 +0100

    About page styling complete

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -3,13 +3,33 @@ import Image from "next/image";
 
 const AboutPage = () => {
   return (
-    <div>
+    <div className={styles.container}>
+      <div className={styles.textContainer}>
+        <h2 className={styles.subtitle}>About this app PLACEHOLDER</h2>
+        <h1 className={styles.title}>HERO TEXT PLACEHOLDER</h1>
+        <p className={styles.desc}>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum rerum
+          nostrum quaerat, nam tempora aspernatur fugiat ducimus ipsa nulla eos,
+          accusantium error dolorum, cupiditate qui magni esse provident ex
+          repudiandae.
+        </p>
+        <div className={styles.boxes}>
+          <div className={styles.box}>
+            <h1>TRIVIA #1</h1>
+            <p>About trivia #1</p>
+          </div>
+          <div className={styles.box}>
+            <h1>TRIVIA #2</h1>
+            <p>About trivia #2</p>
+          </div>
+          <div className={styles.box}>
+            <h1>TRIVIA #3</h1>
+            <p>About trivia #3</p>
+          </div>
+        </div>
+      </div>
       <div className={styles.imgContainer}>
-        <Image
-          src="https://images.pexels.com/photos/1252890/pexels-photo-1252890.jpeg?auto=compress&cs=tinysrgb&w=400"
-          alt="About page image"
-          fill
-        />
+        <Image src="/about.png" alt="" fill className={styles.img} />
       </div>
     </div>
   );
