eb30dbceef81f17f99d9d02d9e25ebd3fd884860
gritNgo <dustspeck00@gmail.com>
2024-01-20 17:13:05 +0100

    Added responsiveness to pages

--- src/app/layout.js
+++ src/app/layout.js
@@ -15,7 +15,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={inter.className}>
         <div className='container'>
-        <Navbar />
+        <Navbar />  
         {children}
         <Footer />
         </div>
