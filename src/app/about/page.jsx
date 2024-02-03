ecdadb52e8e2029d1f4776d678aa4acfc2a0b863
Fiorenso Fernando <dustspeck00@gmail.com>
2024-02-03 11:34:01 +0000

    About page styling and text updates complete

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -3,78 +3,90 @@ import styles from "./about.module.css";
 const AboutPage = () => {
   return (
     <div className={styles.container}>
-      <div className={styles.textContainer}>
-        <div className={styles.desc}>
-          <p>I chose web development as my career only in 2023.</p>
-          <p>
-            As I evidently lack years of expertise that many other web
-            developers have, I have been working double time to close this gap
-            by constantly improving. This first version of the project serves 3
-            demonstrative purposes:{" "}
-          </p>
-        </div>
+      <div className={styles.main}>
         <div className={styles.stackContainer}>
-          <h2>TECHNOLOGIES I'VE WORKED WITH</h2>
+          <div className={styles.stackTitle}>TECHNOLOGY I USE</div>
           <div className={styles.stack}>
-            <div>
-              <img src="" alt="" />
+            <div className={styles.logo}>
+              <img src="/svg/nextjs.svg" alt="Next.js" width="50" height="50" />
             </div>
-            <div>
-              <img src="" alt="" />
+            <div className={styles.logo}>
+              <img src="/svg/nodejs.svg" alt="Node.js" width="50" height="50" />
             </div>
-            <div>
-              <img src="" alt="" />
+            <div className={styles.logo}>
+              <img src="/svg/react.svg" alt="React" width="50" height="50" />
             </div>
-            <div>
-              <img src="" alt="" />
+            <div className={styles.logo}>
+              <img
+                src="/svg/mongodb.svg"
+                alt="MongoDB"
+                width="50"
+                height="50"
+              />
             </div>
-            <div>
-              <img src="" alt="" />
+            <div className={styles.logo}>
+              <img src="/svg/mysql.svg" alt="MySQL" width="50" height="50" />
             </div>
-            <div>
-              <img src="" alt="" />
+            <div className={styles.logo}>
+              <img
+                src="/svg/javascript.svg"
+                alt="JavaScript"
+                width="50"
+                height="50"
+              />
             </div>
-            <div>
-              <img src="" alt="" />
+            <div className={styles.logo}>
+              <img src="/svg/css.svg" alt="CSS" width="50" height="50" />
             </div>
-            <div>
-              <img src="" alt="" />
+            <div className={styles.logo}>
+              <img src="/svg/html.svg" alt="HTML5" width="50" height="50" />
             </div>
-            <div>
-              <img src="" alt="" />
+            <div className={styles.logo}>
+              <img src="/svg/linux.svg" alt="Linux" width="50" height="50" />
             </div>
-            <div>
-              <img src="" alt="" />
-            </div>
-            <div>
-              <img src="" alt="" />
+            <div className={styles.logo}>
+              <img src="/svg/git.svg" alt="Git" width="50" height="50" />
             </div>
           </div>
         </div>
+      <div className={styles.textContainer}>
+        <div className={styles.wrapper}>
+          <div className={styles.aboutTitle}>ABOUT THIS WEBSITE</div>
+          <div className={styles.intro}>
+            I chose web development as my career only in 2023.
+          </div>
+          <div className={styles.desc}>
+            As I evidently lack years of expertise that many other web
+            developers have, I am working double time to close this gap
+            by constantly improving. This first version of the project serves{" "}
+            <b> 3 demonstrative purposes: ---{'>'}</b>
+          </div>
+        </div>
+      </div>
       </div>
       <div className={styles.boxes}>
         <div className={styles.box}>
-          <h3>Portfolio</h3>
-          <p>To showcase my work to date</p>
+          <b>
+            <h3>1) Portfolio</h3>
+          </b>
+          <p>To showcase my current and future work.</p>
         </div>
         <div className={styles.box}>
-          <h3>REST API: Fetching data</h3>
+          <b>
+            <h3>2) REST API: Fetching data</h3>
+          </b>
           <p>
             The Blog page fetches fake data from the <i>JSONPlaceholder</i>{" "}
-            website, as is usually done when prototyping.
+            website, as is usually done for prototyping products.
           </p>
         </div>
         <div className={styles.box}>
-          <h3>REST API: Authentication</h3>
+          <b>
+            <h3>3) REST API: GitHub Authentication</h3>
+          </b>
           <p>
-            GitHub authentication is integrated thanks to Auth.js, which
-            automatically handles OAuth callbacks. Posts can be viewed only if
-            logged in (logged in by default for demo), but only admin can
-            create/update/delete posts.
-          </p>
-          <p>
-            (If you like to see for yourself use: username = John, password =
-            password)
+            Integrated thanks to Auth.js, which automatically handles OAuth
+            callbacks.
           </p>
         </div>
       </div>
--- src/app/portfolio/page.jsx
+++ src/app/portfolio/page.jsx
@@ -10,6 +10,15 @@ const PortfolioPage = () => {
   return (
     <div>
       <div>PROJECTS</div>
+      <div>Portfolio</div>
+      <p>Posts can be viewed only if
+            logged in (logged in by default for demo), but only admin can
+            create/update/delete posts.
+          </p>
+          <p>
+            (If you like to see for yourself use: username = John, password =
+            password)
+          </p>
     </div>
   );
 };
