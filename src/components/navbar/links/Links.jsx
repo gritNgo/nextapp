63cd3ffa5534c4782de17e778891389bde91beb1
Fiorenso Fernando <dustspeck00@gmail.com>
2024-02-07 03:03:51 +0000

    Added navbar media queries for all screen sizes

--- src/components/navbar/links/Links.jsx
+++ src/components/navbar/links/Links.jsx
@@ -1,5 +1,5 @@
 "use client";
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 import styles from "./links.module.css";
 import NavLink from "./navLink/navLink";
 import Image from "next/image";
@@ -7,6 +7,10 @@ import { handleLogout } from "@/lib/action";
 import { auth } from "@/lib/auth";
 
 const links = [
+  {
+    title: "Home",
+    path: "/",
+  },
   {
     title: "About",
     path: "/about",
@@ -24,6 +28,7 @@ const links = [
 const Links = ({ session }) => {
   const [open, setOpen] = useState(false);
 
+
   return (
     <div className={styles.container}>
       <a
@@ -35,10 +40,35 @@ const Links = ({ session }) => {
         <span className={styles.bar}></span>
         <span className={styles.bar}></span>
       </a>
+      <div className={styles.links}>
+        {links.map((link) => (
+          <NavLink item={link} key={link.title} />
+        ))}
+        {session?.user ? (
+          <>
+            {session.user?.isAdmin && (
+              <NavLink item={{ title: "Admin", path: "/admin" }} />
+            )}
+            <form action={handleLogout}>
+              <button className={styles.logout}>Logout</button>
+            </form>
+          </>
+        ) : (
+          <NavLink item={{ title: "Login", path: "/login" }} />
+        )}
+      </div>
+      {/* <Image
+        className={styles.menuButton}
+        src="/menu.png"
+        alt=""
+        width={30}
+        height={30}
+        onClick={() => setOpen((prev) => !prev)}
+      /> */}
       {open && (
-        <div className={styles.links}>
+        <div className={styles.mobileLinks} onClick={() => setOpen((prev) => !prev)}>
           {links.map((link) => (
-            <NavLink item={link} key={link.title} className={styles.link} />
+            <NavLink item={link} key={link.title}  />
           ))}
           {session?.user ? (
             <>
@@ -54,33 +84,6 @@ const Links = ({ session }) => {
           )}
         </div>
       )}
-      {/* {open && (
-        <div className={styles.links}>
-          {links.map((link) => (
-            <NavLink item={link} key={link.title} />
-          ))}
-          {session?.user ? (
-            <>
-              {session.user?.isAdmin && (
-                <NavLink item={{ title: "Admin", path: "/admin" }} />
-              )}
-              <form action={handleLogout}>
-                <button className={styles.logout}>Logout</button>
-              </form>
-            </>
-          ) : (
-            <NavLink item={{ title: "Login", path: "/login" }} />
-          )}
-        </div>
-      )} */}
-      {/* <Image
-        className={styles.menuButton}
-        src="/menu.png"
-        alt=""
-        width={30}
-        height={30}
-        onClick={() => setOpen((prev) => !prev)}
-      /> */}
     </div>
   );
 };
