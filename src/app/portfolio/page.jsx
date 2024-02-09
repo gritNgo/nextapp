51d7d04f8b92a6fc809fb9e6c591ac23f5469bd5
Fiorenso Fernando <dustspeck00@gmail.com>
2024-02-09 20:49:01 +0000

    Updated text

--- src/app/portfolio/page.jsx
+++ src/app/portfolio/page.jsx
@@ -11,100 +11,103 @@ const PortfolioPage = () => {
   return (
     <div className={styles.container}>
       <div className={styles.projects}>
-        
-          <div className={styles.card}>
-            <a href="#" className={styles.link}>
-              <Image
-                className={styles.image}
-                src="/thumbnails/portfolioThumbnail.png"
-                alt=""
-                width={420}
-                height={210}
-              />
+        <div className={styles.card}>
+          <a href="#" className={styles.link}>
+            <Image
+              className={styles.image}
+              src="/thumbnails/portfolioThumbnail.png"
+              alt=""
+              width={420}
+              height={210}
+            />
+          </a>
+          <div className={styles.desc}>
+            <a href="#">
+              <h3 className={styles.title}>Portfolio</h3>
             </a>
-              <div className={styles.desc}>
-                <a  href="#"><h3 className={styles.title}>Portfolio</h3></a>
-                <ul>
-                  <li>
-                    <p className={styles.stack}>
-                      Next.js 14 full-stack app using app router, server
-                      actions, middleware, MongoDB, and Auth.js.
-                    </p>
-                  </li>
-                  <li>
-                    Posts can be viewed only if logged in (with arbitrary credentials or GitHub) but{" "}
-                    <i>only admin</i> can create/update/delete posts (username: john, password: 123456).
-                  </li>
-                  <li>
-                    These are fetched using the <b>JSONPlaceholder</b> website's
-                    fake data REST API which works for all posts and single post
-                    pages.
-                  </li>
-                </ul>
-              </div>
+            <ul>
+              <li>
+                <p className={styles.stack}>
+                  Next.js 14 full-stack app using app router, server actions,
+                  middleware, MongoDB, and Auth.js.
+                </p>
+              </li>
+              <li>
+                Posts can be viewed only if logged in (with arbitrary
+                credentials (username: john, password: 123456) or GitHub) but{" "}
+                <i>only admin</i> can create/update/delete posts .
+              </li>
+              <li>
+                These are fetched using the <b>JSONPlaceholder</b> website's
+                fake data REST API which works for all posts and single post
+                pages.
+              </li>
+            </ul>
           </div>
-        
-        
-          <div className={styles.card}>
-            <a href="#" className={styles.link}>
-              <Image
-                className={styles.image}
-                src="/thumbnails/yakoThumbnail.png"
-                alt=""
-                width={420}
-                height={210}
-              />
+        </div>
+
+        <div className={styles.card}>
+          <a href="#" className={styles.link}>
+            <Image
+              className={styles.image}
+              src="/thumbnails/yakoThumbnail.png"
+              alt=""
+              width={420}
+              height={210}
+            />
+          </a>
+          <div className={styles.desc}>
+            <a href="#">
+              <h3 className={styles.title}>Yako (E-Commerce Store)</h3>
             </a>
-              <div className={styles.desc}>
-                <a  href="#"><h3 className={styles.title}>Yako (E-Commerce Store)</h3></a>
-                <ul>
-                  <li>
-                    <p className={styles.stack}>
-                      Full stack MERN shopping application.
-                    </p>
-                  </li>
-                  <li>
-                    Once shopping cart has been filled and after logging in, user can make payments thanks to Stripe
-                    API. Firebase is used as storage of users and products.
-                  </li>
-                  <li>Still in production, click to preview.</li>
-                </ul>
-              </div>
+            <ul>
+              <li>
+                <p className={styles.stack}>
+                  Full stack MERN shopping application.
+                </p>
+              </li>
+              <li>
+                Once shopping cart has been filled and after logging in, user
+                can make payments thanks to Stripe API. Firebase is used as
+                storage of users and products.
+              </li>
+              <li>Still in production, click to preview.</li>
+            </ul>
           </div>
-        
-        
-          <div className={styles.card}>
-            <a href="#" className={styles.link}>
-              <Image
-                className={styles.image}
-                src="/thumbnails/catchballThumbnail.png"
-                alt=""
-                width={420}
-                height={210}
-                />
-                </a>
-              <div className={styles.desc}>
-                <a  href="#"><h3 className={styles.title}>Catchball (JavaScript Game)</h3></a>
-                <ul>
-                  <li>
-                    <p className={styles.stack}>
-                      Simple 2D game in Vanilla JavaScript.{" "}
-                    </p>
-                  </li>
-                  <li>
-                    Characters throw the ball to each other using
-                    mouse-click/press-hold drag. If the other catches, thrower
-                    wins.
-                  </li>
-                  <li>
-                    JavaScript is used draw the characters and background on a
-                    canvas element.
-                  </li>
-                  <li>Still in production, click to preview.</li>
-                </ul>
-              </div>
+        </div>
+
+        <div className={styles.card}>
+          <a href="#" className={styles.link}>
+            <Image
+              className={styles.image}
+              src="/thumbnails/catchballThumbnail.png"
+              alt=""
+              width={420}
+              height={210}
+            />
+          </a>
+          <div className={styles.desc}>
+            <a href="#">
+              <h3 className={styles.title}>Catchball (JavaScript Game)</h3>
+            </a>
+            <ul>
+              <li>
+                <p className={styles.stack}>
+                  Simple 2D game in Vanilla JavaScript.{" "}
+                </p>
+              </li>
+              <li>
+                Characters throw the ball to each other using
+                mouse-click/press-hold drag. If the other catches, thrower wins.
+              </li>
+              <li>
+                JavaScript is used draw the characters and background on a
+                canvas element.
+              </li>
+              <li>Still in production, click to preview.</li>
+            </ul>
           </div>
-        
+        </div>
       </div>
     </div>
   );
